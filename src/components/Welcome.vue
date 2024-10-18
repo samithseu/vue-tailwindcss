b5ba8344cd82c136bcbc7e93e28a79beecd8104c
Samith <samithseu2002@gmail.com>
2024-10-19 02:43:56 +0700

    initial commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="w-dvw min-h-dvh bg-[#020905] flex flex-col gap-8 items-center justify-center selection:bg-[#41B883] selection:text-white"
+  >
+    <img
+      class="drop-shadow-[0_0_35px_rgba(65,184,131,0.3)] transition-all hover:drop-shadow-[0_0_50px_rgba(65,184,131,0.5)] duration-200 size-[180px]"
+      src="/src/assets/vue.svg"
+      alt="Vue Logo"
+    />
+    <h2 class="text-green-300 text-center font-semibold text-2xl">
+      This project created for
+      <span class="font-mono"> Vue + TailwindCSS</span>.
+    </h2>
+    <h2 class="text-green-300 text-center font-semibold text-2xl">
+      Project នេះបង្កើតឡើងសម្រាប់ការប្រើប្រាស់
+      <span class="font-mono"> Vue + TailwindCSS</span>
+    </h2>
+  </div>
+</template>
+
+<script setup></script>

73a0bdaa1ed6e424a5b89ab74f43da3e643635d3
samithseu <samithseu2002@gmail.com>
2025-11-02 23:25:19 +0700

    [chore]: removed `.vscode`

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -11,7 +11,9 @@
     />
     <h2 class="text-green-300 text-center font-semibold text-2xl">
       This <span class="font-mono">template</span> created for
-      <span class="font-mono"> Vue + TailwindCSS</span>.
+      <span class="font-mono">
+        Vue + <span class="text-[#00bcff]">TailwindCSS</span></span
+      >.
     </h2>
   </div>
 </template>
